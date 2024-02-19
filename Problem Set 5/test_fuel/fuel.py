7e0a3374219e7f427bf41146aa6deaaa6b5622c5
Simon Rohrer <simon.rohrer@live.com>
2024-02-19 13:16:00 +0000

    ps5

--- Problem Set 5/test_fuel/fuel.py
+++ Problem Set 5/test_fuel/fuel.py
@@ -1,34 +1,34 @@
 def main():
     while True:
         fraction = input("Fraction: ")
-        fraction = fraction.split('/')
-
-        try:
-            fuel = int(fraction[0]) / int(fraction[1])
-            if int(fraction[0]) > int(fraction[1]) or int(fraction[0]) < 0:
-                continue
-        except ValueError:
-            pass
-        except ZeroDivisionError:
-            pass
-        else:
-            fuel = round(fuel * 100)
-
-            if fuel <= 1:
-                print("E")
-            elif fuel >= 99:
-                print("F")
-            else:
-                print(f"{fuel}%")
-            break
+        percentage = convert(fraction)
+        print (gauge(percentage))
 
 
 def convert(fraction):
-    ...
+    fraction = fraction.split('/')
+
+    try:
+        fuel = int(fraction[0]) / int(fraction[1])
+        #if int(fraction[0]) > int(fraction[1]) or int(fraction[0]) < 0:
+        #    continue
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+
+    return fuel
 
 
 def gauge(percentage):
-    ...
+    percentage = round(percentage * 100)
+
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return (f"{percentage}%")
 
 
 if __name__ == "__main__":
--- Problem Set 5/test_fuel/test_fuel.py
+++ Problem Set 5/test_fuel/test_fuel.py
@@ -1,1 +1,4 @@
 from fuel import convert, gauge
+
+def test_convert_valueerror():
+    assert convert('5/3')
