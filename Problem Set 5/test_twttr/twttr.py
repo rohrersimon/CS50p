636bc6fae9ddd0a83b87b505c5a2770fd53c8af8
Simon Rohrer <simon.rohrer@live.com>
2024-02-19 07:12:11 +0000

    Problem Set 5

---
+++ Problem Set 5/test_twttr/twttr.py
@@ -0,0 +1,18 @@
+def main():
+    word = input('Input: ')
+    shorten(word)
+    print(f'Output: {word}')
+
+
+def shorten(word):
+    for letter in word:
+        match letter.lower():
+            case 'a' | 'e' | 'i' | 'o' | 'u':
+                word = word.replace(letter, '')
+            case _:
+                continue
+    return word
+
+
+if __name__ == "__main__":
+    main()
