183b96f3c157b53e8d1ff520ac920373a55a8ad5
Simon Rohrer <simon.rohrer@live.com>
2024-02-21 10:22:44 +0000

    ps6

---
+++ Problem Set 6/lines/pizza.py
@@ -0,0 +1,26 @@
+import sys
+from os import path
+import csv
+from tabulate import tabulate
+
+
+# Handle command-line arguments
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+csvname = sys.argv[1]
+
+if not path.exists(csvname):
+    sys.exit("File does not exist")
+elif not csvname.endswith('.csv'):
+    sys.exit("Not a csv file")
+
+# Open CSV file
+with open(csvname, 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+# Print ACSII art
+print(tabulate(data, tablefmt="grid"))
