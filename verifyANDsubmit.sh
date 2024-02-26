d4da70c80cc4d3ce9dcfa108629e9cb4f4acb9f9
Simon Rohrer <simon.rohrer@live.com>
2024-02-26 17:03:15 +0000

    c

--- verifyANDsubmit.sh
+++ verifyANDsubmit.sh
@@ -1,22 +1,20 @@
-# Define the array of exercise names
 # exercises=("einstein" "faces" "indoor" "playback" "tip")
 # exercises=("deep" "bank" "extensions" "interpreter" "meal")
 # exercises=("camel" "coke" "twttr" "plates" "nutrition")
 # exercises=("fuel" "taqueria" "grocery" "outdated")
 # exercises=("emojize" "figlet" "adieu" "game" "professor" "bitcoin")
-exercises=("bank" "fuel" "plates" "twttr") #tests
-ProblemSet='5'
+# exercises=("bank" "fuel" "plates" "twttr")
+# exercises=("lines" "pizza" "scourgify" "shirt")
+exercises=("numb3rs" "um" "watch" "working" "response")
+ProblemSet='7'
 
 # Loop through the exercises and check each exercise
 for string in "${exercises[@]}"; do
-    # location
-    cd /workspaces/105282514/'Problem Set '$ProblemSet/'test_'$string
+    cd /workspaces/105282514/'Problem Set '$ProblemSet/$string
 
-    # Check or submit
-    check50 cs50/problems/2022/python/tests/$string
-    #submit50 cs50/problems/2022/python/$string
+    #check50 cs50/problems/2022/python/$string
+    submit50 cs50/problems/2022/python/$string
 
-    # Pause after one submission
-    read -p "Press Enter to continue... "
+    #read -p "Press Enter to continue... "
 
 done
